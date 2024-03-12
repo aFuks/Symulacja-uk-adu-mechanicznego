cb533ca9e45943ed8506a1ecbc03b24e56b07c1c
Aleksander Fuks <96986297+aFuks@users.noreply.github.com>
2024-03-12 19:34:29 +0100

    Create Projekt_MMM.py

---
+++ Projekt_MMM.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import tkinter.messagebox
+import matplotlib.pyplot as plt
+import numpy as np
+
+n1 = 1  # zębatka 1
+n2 = 1  # zębatka 2
+j1 = 1  # wał 1
+j2 = 1  # wał 2
+k = 1  # spręzyna
+b = 1  # tłumik
+h = 0.1  # stała całkowania
+
+# listy z obliczonymi przez alhgorytm wartościami
+x1 = []
+x2 = []
+x1euler = []
+x2euler = []
